4d094dc74b3f4a35bb63cc491e86f235557a76c7
yuyuyu0914 <1851636431@qq.com>
2024-08-06 15:50:36 +0800

    完成注册页面

--- js/common.js
+++ js/common.js
@@ -1,2 +1,15 @@
 //配置axios基地址(全局默认值)
 axios.defaults.baseURL = 'https://hmajax.itheima.net';
+
+//公共的提示框函数
+const showToast = (msg) => {
+    // alert(msg)
+    //生成实例对象
+    const mytoast=document.querySelector('.my-toast')
+    const toastObj=new bootstrap.Toast(mytoast)
+    toastObj.show()
+    //2.2让提示框组件内容改变
+    document.querySelector('.toast-body').innerHTML=msg 
+
+}
+// showToast('成功1')
---
+++ js/register.js
@@ -0,0 +1,29 @@
+/**
+ * 用户登录
+ *  1. 收集表单数据并判断
+ *  2. 数据提交
+ *  3. 提示用户
+ *  4. 登录成功（缓存数据+页面跳转）
+ * */
+document.querySelector('#btn-register').addEventListener('click', async function (e) {
+    const data=serialize(document.querySelector('.register-form'),{hash:true,empty:true})
+    if(!data.username){
+        return showToast('用户名不能为空')
+    }
+    if(!data.password){
+        return showToast('密码不能为空')
+    }
+    if(data.username.length<8||data.username.length>30){
+        return showToast('用户名不符合要求，用户名长度必须在8-30之间')
+    }
+    if(data.password.length<6||data.password.length>30){
+        return showToast('密码不符合要求，密码长度必顼在6-30之间')
+    }
+    //发送请求
+    const res=await axios.post('/register',data)
+    console.log(res)
+    showToast(res.data.message)
+    setTimeout(()=>{
+        location.href='./login.html'
+    },2000)
+})
